0b9d0f56ebb8a4b10ea5e403b8054c1a5d8c9141
Davi Placco <davibplacco@gmail.com>
2024-04-01 16:15:24 +0100

    Delete: Remove SVG vite logo

---
+++ src/components/Btn.tsx
@@ -0,0 +1,12 @@
+
+export const Btn = () =>{
+
+    return(
+        <a target="_blank" href="https://google.com">
+            <button className="bg-green-400 w-48 px-2 py-4 rounded-xl hover:bg-green-700 hover:text-white transition duration-300">
+                Check the Link
+            </button>
+        </a> 
+    )
+
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,16 @@
+import {Btn} from '../components/Btn'
 
 export const Home = () => {
 
     return(
-    <section className="">
+    <section className="flex flex-col w-auto h-128 justify-center items-center gap-8">
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
         <h2 className="text-lg text-gray">
             Test H2
         </h2>
+        <Btn/>
     </section>
     )
 }
